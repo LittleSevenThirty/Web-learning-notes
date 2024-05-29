fa838b072765f9b31666f05db69f23af237d90f8
Zhongcream <3100649706@qq.com>
2024-05-29 22:36:43 +0800

    添加Generator函数的一些内容

--- 05-javaScriptES6进阶版/Iterator的概念/05-for_of的使用.js
+++ 05-javaScriptES6进阶版/Iterator的概念/05-for_of的使用.js
@@ -33,3 +33,45 @@ for (let a in arr2) {
 for (let b of arr2) {
     console.log(b); // a b c d
 }
+
+console.log("--------------------");
+
+// Set 和 Map 结构
+// Set 和 Map 结构也原生具有 Iterator 接口，可以直接使用for...of循环。
+
+// 类似数组的对象包括好几类。for...of循环可以用于字符串、DOM NodeList 对象、arguments对象
+
+// 并不是所有类似数组的对象都具有 Iterator 接口，一个简便的解决方法，就是使用Array.from方法将其转为数组。
+let arrayLike = { length: 2, 0: 'a', 1: 'b' };
+// 如果使用以下代码是会出错的
+// for (let x of arrayLike) {
+//     console.log(x);
+// }
+// 原因是arrayLike对象没有部署Symbol.iterator属性，所以不能使用for...of循环。
+for (let x of Array.from(arrayLike)) {
+    console.log(x);
+}
+
+
+// 对于普通的对象，for...of结构不能直接使用，会报错，必须部署了 Iterator 接口后才能使用。但是，这样情况下，for...in循环依然可以用来遍历键名。
+
+let es6 = {
+    edition: 6,
+    committee: "TC39",
+    standard: "ECMA-262"
+};
+for (let e in es6) {
+    console.log(e);
+}
+// edition
+// committee
+// standard
+for (let e of es6) {
+    console.log(e);
+}
+// TypeError: es6[Symbol.iterator] is not a function
+
+// 解决方法1：部署独属于对象的iterator接口或者使用Generator函数重新包装一下
+
+// 解决方法2: 并不是所有类似数组的对象都具有 Iterator 接口，一个简便的解决方法，就是使用Array.from方法将其转为数组。   
+
